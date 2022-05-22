1f7254913f14b9706366610d4fe52776100d0aff
george <george_dyatchkov@mail.ru>
2022-05-22 20:40:29 +0300

    sdsdsds

--- main.py
+++ main.py
@@ -1,30 +1,42 @@
-from flask import Flask, render_template, redirect, url_for, request
+import flask
+import json
+
+from flask import Flask, render_template, redirect, url_for, request, jsonify
 from flask_login import login_user, LoginManager, login_required, current_user
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.utils import secure_filename
 from wtforms import StringField, PasswordField,SubmitField, IntegerField, Form
 from flask_wtf import FlaskForm
 from flask_socketio import SocketIO, send
 from sqlalchemy import func
+import base64
+import os
 app= Flask(__name__)
-
+UPLOAD_FOLDER = 'static\path'
 app.secret_key = "super secret key"
 app.config["SESSION_PERMANENT"] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SECRET_KEY']='thisissecretkey'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 db = SQLAlchemy(app)
 socketio = SocketIO(app)
-
-
+socketid=[]
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 login_manager=LoginManager()
 login_manager.init_app(app)
 login_manager.login_view="login"
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 class ChatMessages(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(256))
+    username=db.Column(db.String(256))
     msg = db.Column(db.Text)
     chat = db.Column(db.Integer)
+    data=db.Column(db.Integer)
+    filename=db.Column(db.String(256), default='no')
 
 
 class Chat(db.Model):
@@ -39,6 +51,8 @@ db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
 db.Column('friend_id', db.Integer, db.ForeignKey('user.id'))
 )
 
+
+users={}
 
 class User(db.Model):
     id=db.Column(db.Integer,primary_key=True)
@@ -151,6 +165,47 @@ def login():
     return render_template("login.html")
 
 
+@app.route("/filehandler", methods=["GET", "POST"])
+def filehandler():
+    print('sdsds')
+    if request.method == 'POST':
+        print('sdsds')
+        print(request.form.get('chat'))
+
+        messmax = ChatMessages.query.with_entities(ChatMessages.data).all()
+        print('messsmax'+str(messmax))
+        if len(messmax) == 0:
+            max_logins = 0
+            max_logins += 1
+        else:
+            max_logins = max(messmax)
+            max_logins = max_logins[0] + 1
+        print('succse')
+
+        data=int(request.form.get('chat'))
+        userr = str(request.form.get('user'))
+        file = request.files['file']
+        #print(file.filename)
+        #msg = base64.b64encode(file.read())
+        #print(file.read())
+        filename=str(max_logins)+'.'+str(file.filename.split('.')[-1])
+
+        print(filename)
+
+
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+        message = ChatMessages(username=userr, msg='файл', chat=data,data=max_logins, filename=filename)
+        db.session.add(message)
+
+        db.session.commit()
+        jsonobj={'filename':filename}
+        print('did it')
+        print( jsonify(jsonobj))
+        
+        return {'dsdsd':'sdds'}
+
+
 
 @app.route("/createchat", methods=["GET", "POST"])
 def createchat():
@@ -184,12 +239,23 @@ def createchat():
 
     return render_template('createchat.html',chatnumber=chatnumber)
 
-
+@app.route("/delete/<int:chat_id>", methods=["GET", "POST"])
+def delete(chat_id):
+    message=ChatMessages.query.get(chat_id)
+    db.session.delete(message)
+    db.session.commit()
+    return 'ok'
 
 @app.route("/main/<int:chat_id>", methods=["GET", "POST"])
 def main(chat_id):
     chat=Chat.query.filter_by(chat_id=chat_id).with_entities(Chat.User_id).all()
     list=[x[0] for x in chat]
+    listt = []
+    for i in list:
+        chatt=User.query.filter_by(id=i).with_entities(User.email).all()
+        listt.append(chatt[0][0])
+
+
 
 
     print(list)
@@ -204,20 +270,89 @@ def main(chat_id):
 
         messages=ChatMessages.query.filter_by(chat=chat_id)
 
+        imageslist=[]
+
+
+
+
     print(user)
 
-    return render_template('main.html',chat_id=chat_id, messages=messages, user=user)
+    return render_template('main.html',chat_id=chat_id, messages=messages, user=user, friends=listt)
 @socketio.on('message')
 def handleMessage(data):
+    global users
+
+    print('реквест айди отправителя'+str(request.sid))
+
+
+
     print(data)
     if current_user.is_authenticated:
-        send(data, broadcast=True)
-        print(data['base64String'])
-        message = ChatMessages(username=current_user.email, msg=data['msg'], chat=data['chat'])
-        db.session.add(message)
-        db.session.commit()
+        user=str(data['user']).strip()
+        print(user)
+        userr=User.query.filter_by(email=user).one()
+        print(userr)
+        chat=int(data['chat'])
+        chat_list = Chat.query.with_entities(Chat.User_id).filter_by(chat_id=chat).all()
+        chat_list=[x[0] for x in chat_list]
+        print(chat_list)
+        print(data['friends'])
+        friendd=data['friends'].replace(']','').replace('[','').replace("'","").replace('\n','').split(',')
+        friendd=[x.strip() for x in friendd]
+        print(friendd)
+        print(users)
 
 
+
+        if userr.id not in chat_list:
+            pass
+        else:
+            for i in friendd:
+                print(i)
+
+                if i in users[chat]:
+                    print(users[chat][i])
+                    sisid=users[chat][i]
+                    print(sisid)
+                    send(data,to=sisid)
+            messmax = ChatMessages.query.with_entities(ChatMessages.data).all()
+            print( messmax)
+            if len( messmax) == 0:
+                max_logins = 0
+                max_logins += 1
+            else:
+                max_logins = max(messmax)
+                max_logins = max_logins[0] + 1
+
+            #databinary = base64.b64decode(data['base64String'])
+
+            message = ChatMessages(username=current_user.email, msg=data['msg'], chat=data['chat'],data=max_logins)
+            db.session.add(message)
+
+            db.session.commit()
+
+
+@socketio.on('username', namespace='/private')
+def recieve_username(privatem):
+    print('получиличчч'+str(privatem))
+    user=current_user.email
+    print(user)
+    chat=int(privatem['chat'])
+    print(chat)
+
+    if chat not in users:
+        users[chat]={user:privatem['sisid']}
+    else:
+        new_pairs=[ (user,privatem['sisid'])]
+        users[chat].update(new_pairs)
+
+
+
+
+
+    print(users)
+
+    print('получили айд '+str(privatem))
 
 
 
